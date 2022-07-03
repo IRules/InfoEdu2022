21c93eb315ce64dabe2d35e3ee9235698ffc051d
IRules <irulesmain@gmail.com>
2022-07-03 04:32:21 +0300

    Fixed an error.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,20 +1,3 @@
-import admin from '@/lib/firebase'
+import admin from '@/lib/firebase';
 
-export default async function handler(req, res) {
-    const firebase = admin.firestore()
-
-    // Return promise to handle serverless function timeouts
-    return new Promise((resolve, reject) => {
-       firebase.then(() => {
-          res.status(200).json({ data })
-          res.end()
-          resolve()
-        })
-        .catch((e) => {
-          res.status(405).json(e)
-          res.end()
-          resolve()
-        })
-    }
-
-}
+export default async function handler(req, res) {}
