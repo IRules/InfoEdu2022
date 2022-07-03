4c86c60a40778c686df700689f7d71ff780291c7
IRules <irulesmain@gmail.com>
2022-07-04 02:01:32 +0300

    Fixed a bug in Account page.

--- pages/account/index.js
+++ pages/account/index.js
@@ -46,7 +46,83 @@ function Settings() {
           setName(user.data().name);
         }
       );
-    }, 1000);
+      return (
+        <Fade>
+          <Navbar navstyle={true} />
+          <div className={styles.settings}>
+            <div className={styles.settings__container}>
+              <div className={styles.settings__containerAccount}>
+                <div className={styles.settings__containerAccountPfp}>
+                  <Avatar
+                    sx={{ width: 126, height: 126 }}
+                    src={`https://robohash.org/${email}?set=set4`}
+                  />
+                </div>
+                <div className={styles.settings__containerAccountName}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Nume"
+                    value={name}
+                    variant="outlined"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className={styles.settings__containerAccountInfo}>
+                  <Alert severity="info" sx={{ ml: '20px' }}>
+                    Aceste date sunt utilizte pentru sectiunea de comentarii si
+                    cea de chat.
+                  </Alert>
+                </div>
+              </div>
+
+              <div className={styles.settings__containerPreferences}>
+                <Alert severity="info" sx={{ ml: '20px' }}>
+                  Aceste date sunt preluate din contul tau google.
+                </Alert>
+                <FormControl fullWidth>
+                  <InputLabel>Buget</InputLabel>
+                  <Select value={budget} label="Buget" onChange={handleBudget}>
+                    <MenuItem value={1}>Mic</MenuItem>
+                    <MenuItem value={2}>Mediu</MenuItem>
+                    <MenuItem value={3}>Mare</MenuItem>
+                  </Select>
+                </FormControl>
+                <FormControl fullWidth>
+                  <InputLabel>Medie BAC</InputLabel>
+                  <Select value={bac} label="Medie BAC" onChange={handleBac}>
+                    <MenuItem value={1}>5-7</MenuItem>
+                    <MenuItem value={2}>8-9</MenuItem>
+                    <MenuItem value={3}>9-10</MenuItem>
+                    <MenuItem value={4}>10</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+
+              <div className={styles.settings__containerTest}>
+                <div className={styles.settings__containerTestInfo}>
+                  <Alert severity="info">
+                    Pe aceasta pagina se regasesc informatii despre testul de
+                    cariera.
+                  </Alert>
+                </div>
+                <div className={styles.settings__containerTestProfessions}>
+                  <h1>Profesii:</h1>
+                  <Virtuoso
+                    className={styles.settings__containerTestProfessions}
+                    style={{ height: '400px' }}
+                    totalCount={10}
+                    itemContent={(index) => <Profession />}
+                  />
+                </div>
+                <div className={styles.settings__containerButtons}>
+                  <Button variant="contained">Reda testul de profesie</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Fade>
+      );
+    }, 100);
   }, [auth.currentUser]);
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -69,83 +145,6 @@ function Settings() {
   const handleBac = (event) => {
     setBac(event.target.value);
   };
-
-  return (
-    <Fade>
-      <Navbar navstyle={true} />
-      <div className={styles.settings}>
-        <div className={styles.settings__container}>
-          <div className={styles.settings__containerAccount}>
-            <div className={styles.settings__containerAccountPfp}>
-              <Avatar
-                sx={{ width: 126, height: 126 }}
-                src={`https://robohash.org/${email}?set=set4`}
-              />
-            </div>
-            <div className={styles.settings__containerAccountName}>
-              <TextField
-                id="outlined-basic"
-                label="Nume"
-                value={name}
-                variant="outlined"
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className={styles.settings__containerAccountInfo}>
-              <Alert severity="info" sx={{ ml: '20px' }}>
-                Aceste date sunt utilizte pentru sectiunea de comentarii si cea
-                de chat.
-              </Alert>
-            </div>
-          </div>
-
-          <div className={styles.settings__containerPreferences}>
-            <Alert severity="info" sx={{ ml: '20px' }}>
-              Aceste date sunt preluate din contul tau google.
-            </Alert>
-            <FormControl fullWidth>
-              <InputLabel>Buget</InputLabel>
-              <Select value={budget} label="Buget" onChange={handleBudget}>
-                <MenuItem value={1}>Mic</MenuItem>
-                <MenuItem value={2}>Mediu</MenuItem>
-                <MenuItem value={3}>Mare</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl fullWidth>
-              <InputLabel>Medie BAC</InputLabel>
-              <Select value={bac} label="Medie BAC" onChange={handleBac}>
-                <MenuItem value={1}>5-7</MenuItem>
-                <MenuItem value={2}>8-9</MenuItem>
-                <MenuItem value={3}>9-10</MenuItem>
-                <MenuItem value={4}>10</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
-
-          <div className={styles.settings__containerTest}>
-            <div className={styles.settings__containerTestInfo}>
-              <Alert severity="info">
-                Pe aceasta pagina se regasesc informatii despre testul de
-                cariera.
-              </Alert>
-            </div>
-            <div className={styles.settings__containerTestProfessions}>
-              <h1>Profesii:</h1>
-              <Virtuoso
-                className={styles.settings__containerTestProfessions}
-                style={{ height: '400px' }}
-                totalCount={10}
-                itemContent={(index) => <Profession />}
-              />
-            </div>
-            <div className={styles.settings__containerButtons}>
-              <Button variant="contained">Reda testul de profesie</Button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Fade>
-  );
 }
 
 export default Settings;
